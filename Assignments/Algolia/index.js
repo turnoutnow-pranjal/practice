8287574232e2ce91990f818333d80051f067e005
Pranjal Bhaskar <pranjal.bhaskar@turnoutnow.com>
2022-11-24 11:24:56 +0530

    used .env

--- Assignments/Algolia/index.js
+++ Assignments/Algolia/index.js
@@ -1,15 +1,24 @@
 const algolia=require("algoliasearch")
-const client=algolia("IF25VL36F4","eb59e4b803dd76cbd6869e6cbd3444ab")
-const index=client.initIndex("testData")
+require('dotenv').config();
+let searchKey=process.env.SearchKey
+let appId=process.env.APPID
 
+let indexName="testData"
 const searchTerm="Luna"
 
-index.search(searchTerm).then(({hits})=>{
-    // console.log(hits);
-    console.log("total results: ",hits.length)
-    for (var i=0;i<hits.length;i++){
-        console.log(hits[i].name,"-Price: $",hits[i].price);
-    }
-}).catch((err)=>{
-    console.log(err);
-})
+const client=algolia(appId,searchKey)
+const index=client.initIndex(indexName)
+
+const getResults=()=>{
+    index.search(searchTerm).then(({hits})=>{
+        // console.log(hits);
+        console.log("total results: ",hits.length)
+        for (var i=0;i<hits.length;i++){
+            console.log(hits[i].name,"-Price: $",hits[i].price);
+        }
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+
+getResults()
