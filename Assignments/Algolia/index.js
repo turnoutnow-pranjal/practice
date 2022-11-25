198439cd8b5e0a6a64d6fc76443eca71f38ce03c
Pranjal Bhaskar <pranjal.bhaskar@turnoutnow.com>
2022-11-25 20:16:09 +0530

    implemented filters

--- Assignments/Algolia/index.js
+++ Assignments/Algolia/index.js
@@ -2,23 +2,26 @@ const algolia = require("algoliasearch")
 require('dotenv').config();
 let searchKey = process.env.SearchKey
 const adminKey=process.env.AdminKey
-let appId = process.env.APPID
+const appId = process.env.APPID
 
-let indexName = "testData"
-const searchTerm = "Samsung S22 256GB"
+const indexName = "testData"
+let searchTerm = "Samsung S22 256GB"
+let filterSearchTerm="Samsung"
+let filterRule='price>599.99 AND popularity>20000'
 
 let client = algolia(appId, searchKey)
 let index = client.initIndex(indexName)
+
 const objArray = [
     {
-        objectID:"pranjal-sample-data-157",
-        name:"Samsung S22 256GB Prepaid Cell Phone",
+        objectID:"pranjal-sample-data-158",
+        name:"Samsung S22 256GB GREEN Prepaid Cell Phone",
         brand:"Samsung",
-        price:1000.99,
+        price:1000,
         description:"Such a mast phone! Outrageuos camera and outstanding video processing, ekdum gajab! Too poor battery Life though :/",
-        categories:["Cell Phones", "Prepaid Phones", "All Prepaid Phones"],
+        categories:["Cell Phones", "Prepaid Phones", "All Prepaid Phones","pranjal"],
         image:"https://images.samsung.com/in/smartphones/galaxy-s22/buy/Samsung_Rewards_PC_2x.jpg?$220_N_JPG$",
-        popularity:20000
+        popularity:20001
     }
 ]
 
@@ -40,11 +43,28 @@ const getResults = () => {
 
 }
 
-const pushData=()=>{
+const filteredSearch=()=>{
+    
+    try {
+        index.search(filterSearchTerm,{filters:filterRule}).then(({ hits }) => {
+            // console.log(hits);
+            console.log("total results: ", hits.length)
+            for (var i = 0; i < hits.length; i++) {
+                console.log(hits[i].name, "-Price: $", hits[i].price,"Popularity",hits[i].popularity);
+            }
+        }).catch((err) => {
+            console.log(err);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const pushData=async()=>{
     const client = algolia(appId, adminKey)
     const index = client.initIndex(indexName)
     try {
-        index.saveObjects(objArray)
+        await index.saveObjects(objArray)
         .then(({ objectIDs }) => {
           console.log(objectIDs);
         })
@@ -52,15 +72,15 @@ const pushData=()=>{
           console.log(err);
         });
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
 (
     ()=>{
-        getResults()
+        // getResults()
         // pushData()
-
+        filteredSearch()
     }
 )()
 
